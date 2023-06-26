4612a7232a92e3f86a7a5cdc1f3dcd79eeec9dba
dersiedler92 <dersiedler92@gmail.com>
2023-06-26 23:48:14 +0200

    abend commit

--- src/main/java/org/kickmanager/Main.java
+++ src/main/java/org/kickmanager/Main.java
@@ -5,55 +5,41 @@ import org.kickmanager.player.PlayerGeneration;
 import org.kickmanager.team.Team;
 import org.kickmanager.team.TeamGeneration;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.awt.*;
 import java.util.List;
 
 public class Main {
     private static final PlayerGeneration playerGeneration = new PlayerGeneration();
     private static final TeamGeneration teamGeneration = new TeamGeneration();
 
-    private JPanel mainPanel;
-    private JPanel parentPanel;
-    private JPanel buttonPanel;
-    private JButton button1;
-    private JButton button2;
-    private JPanel card1;
-    private JPanel card2;
-    private JLabel label1;
-    private JLabel label2;
-
     public Main() {
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                parentPanel.removeAll();
-                parentPanel.add(card1);
-                parentPanel.repaint();
-                parentPanel.revalidate();
-            }
-        });
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                parentPanel.removeAll();
-                parentPanel.add(card2);
-                parentPanel.repaint();
-                parentPanel.revalidate();
-            }
-        });
     }
 
     public static void main(String[] args) {
         init();
         JFrame frame = new JFrame("Test");
-        frame.setContentPane(new Main().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(buttonPanel);
+        JButton button1 = new JButton("Hello");
+        buttonPanel.add(button1);
+        JButton button2 = new JButton("Sup");
+        buttonPanel.add(button2);
+
+        JPanel cardPanel1 = new JPanel(new CardLayout());
+        cardPanel1.setSize(500, 500);
+        ImageIcon icon = new ImageIcon("src/main/resources/images/auroras.png");
+        JLabel label1 = new JLabel(icon);
+        cardPanel1.add(label1);
+        mainPanel.add(cardPanel1);
+
+        frame.setContentPane(mainPanel);
         frame.pack();
         frame.setVisible(true);
     }
@@ -65,10 +51,4 @@ public class Main {
         germanSuperLeague.setTeams(superLeagueTeams);
     }
 
-    private void createUIComponents() throws IOException {
-        BufferedImage image1 = ImageIO.read(new File("src/main/resources/images/auroras.png"));
-        BufferedImage image2 = ImageIO.read(new File("src/main/resources/images/bears.png"));
-        label1 = new JLabel(new ImageIcon(image1));
-        label2 = new JLabel(new ImageIcon(image2));
-    }
 }
